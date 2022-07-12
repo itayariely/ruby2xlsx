cd8cfb52921a5f9653304a9460bec73e1f664a50
Itay Ariely <itay@almond-qms.com>
2022-07-12 16:59:53 +0300

    mime type update to rails 5

--- lib/ruby2xlsx/version.rb
+++ lib/ruby2xlsx/version.rb
@@ -1,4 +1,4 @@
 module Ruby2xlsx
-  VERSION = '0.1.1'
+  VERSION = '0.1.2'
     # VERSION = '0.1.0'.freeze
 end
--- lib/ruby2xlsx/xrb.rb
+++ lib/ruby2xlsx/xrb.rb
@@ -2,7 +2,17 @@ module Ruby2xlsx
   class Xrb
     # Default format used by Excel builder.
     class_attribute :default_format
-    self.default_format = ::Mime::XLSX
+    # self.default_format = ::Mime::XLSX
+      def default_format
+        case
+        when Rails.version.to_f >= 6
+          Mime[:xlsx].symbol
+        when Rails.version.to_f >= 5
+          Mime[:xlsx]
+        else
+          Mime::XLSX
+        end
+      end
 
     def call(template)
       %{ controller.send_data ::Ruby2xlsx::Template.new(#{template.source.inspect}).render(self, assigns.merge(local_assigns)),
